1a1da8868602fcd1ad17004f0e1345fd479c0e1a
mcieciora <mcieciora@nowhere.com>
2024-01-25 17:14:01 +0100

    25012024-1713

--- tools/githooks_scripts/check_jenkinsfiles.sh
+++ tools/githooks_scripts/check_jenkinsfiles.sh
@@ -3,7 +3,7 @@
 source .env
 
 JENKINS_FILE_CHECK_FILE=$(mktemp XXXXX)
-find . -iname "*jenkinsfile" > "$JENKINS_FILE_CHECK_FILE"
+find . -name "*Jenkinsfile" > "$JENKINS_FILE_CHECK_FILE"
 
 echo "Found $(cat "$JENKINS_FILE_CHECK_FILE")"
 
@@ -11,7 +11,7 @@ NUMBER_OF_JENKINS_FILES=$(wc -l "$JENKINS_FILE_CHECK_FILE" | awk '{ print $1 }')
 
 echo "Running Jenkinsfiles linting on $JENKINS_SERVER_HOST"
 CURL_LOG_FILE=$(mktemp XXXXX)
-cat < "$JENKINS_FILE_CHECK_FILE" | xargs -I {} curl --user "jenkins_user:pass" -X POST -F "jenkinsfile=<{}" "$JENKINS_SERVER_HOST"/pipeline-model-converter/validate > "$CURL_LOG_FILE"
+cat < "$JENKINS_FILE_CHECK_FILE" | xargs -I {} curl --user "jenkins_user:password" -X POST -F "jenkinsfile=<{}" "$JENKINS_SERVER_HOST"/pipeline-model-converter/validate > "$CURL_LOG_FILE"
 
 NUMBER_OF_OCCURRENCES=$(grep -c "Jenkinsfile successfully validated." "$CURL_LOG_FILE")
 
