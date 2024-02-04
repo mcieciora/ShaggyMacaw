68a4c0e80af387b6afbb219670db53ab354b18dd
mcieciora <mcieciora@nowhere.com>
2024-02-04 13:33:13 +0100

    04022024-1333

--- automated_tests/smoke/test_google_com_status_code.py
+++ automated_tests/smoke/test_google_com_status_code.py
@@ -2,7 +2,8 @@ from pytest import mark
 from src.main import get_google_com_status_code
 
 
-@mark.smoke
-def test__smoke__get_google_com_status_code():
+@mark.unittest
+def test__smoke__get_google_com_status_code(expected_status_code):
     return_status_code = get_google_com_status_code()
-    assert return_status_code == 200, f"Status code check failed. Actual {return_status_code}, Expected: 200"
+    assert return_status_code == expected_status_code, f"Status code check failed. " \
+                                                       f"Actual {return_status_code}, Expected: {expected_status_code}"
