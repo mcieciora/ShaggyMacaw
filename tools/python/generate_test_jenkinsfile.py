4387deac5bb098899d77b09148b99aeeda7154ed
mcieciora <mcieciora@nowhere.com>
2024-02-06 16:24:42 +0100

    06022024-1624

--- tools/python/generate_test_jenkinsfile.py
+++ tools/python/generate_test_jenkinsfile.py
@@ -4,17 +4,17 @@ from sys import argv
 PROJECT_ROOT_PATH = None
 
 
-def _get_template_content():
+def _get_template_content(template_path):
     """
     Get content of template Jenkinsfile.
     :return: Template Jenkinsfile content
     """
-    with open(join(PROJECT_ROOT_PATH, "tools/resources/CarelessVaquitaTestPipelineStageTemplate"),
+    with open(join(PROJECT_ROOT_PATH, template_path),
               mode="r", encoding="utf-8") as template:
         return template.readlines()
 
 
-def _get_injection_index(jenkinsfile_content):
+def _get_injection_index(pattern, jenkinsfile_content):
     """
     Get index of template code injection by finding 'stages' line number in main Jenkinsfile.
     :param jenkinsfile_content: Main Jenkinsfile content
@@ -22,7 +22,7 @@ def _get_injection_index(jenkinsfile_content):
     """
     return_value = 0
     for index, line in enumerate(jenkinsfile_content):
-        if "Prepare docker test image" in line:
+        if pattern in line:
             return_value = index
             break
     return return_value
@@ -36,7 +36,12 @@ def generate_test_jenkinsfile():
     """
     with open(join(PROJECT_ROOT_PATH, "Jenkinsfile"), mode="r", encoding="utf-8") as main_jenkinsfile:
         jenkinsfile_content = main_jenkinsfile.readlines()
-        jenkinsfile_content.insert(_get_injection_index(jenkinsfile_content), "".join(_get_template_content()))
+        jenkinsfile_content.insert(
+            _get_injection_index("Prepare docker test image", jenkinsfile_content),
+            "".join(_get_template_content("tools/resources/TestPipelineStageTemplate")))
+        jenkinsfile_content.insert(
+            _get_injection_index("environment {", jenkinsfile_content),
+            "".join(_get_template_content("tools/resources/TestPipelineParametersTemplate")))
     with open(join(PROJECT_ROOT_PATH, "tools/jenkins/CarelessVaquitaTestJenkinsfile"), mode="w", encoding="utf-8") \
             as test_jenkinsfile:
         test_jenkinsfile.writelines(jenkinsfile_content)
