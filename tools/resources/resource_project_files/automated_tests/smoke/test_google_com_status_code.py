55d7fd0c14ef3caaac2ba228b6a9f49a00ea79bf
mcieciora <mcieciora@nowhere.com>
2024-02-20 20:44:08 +0100

    20022024-2044

--- tools/resources/resource_project_files/automated_tests/smoke/test_google_com_status_code.py
+++ tools/resources/resource_project_files/automated_tests/smoke/test_google_com_status_code.py
@@ -7,6 +7,3 @@ def test__smoke__get_google_com_status_code(expected_status_code):
     return_status_code = get_google_com_status_code()
     assert return_status_code == expected_status_code, f"Status code check failed. " \
                                                        f"Actual {return_status_code}, Expected: {expected_status_code}"
-
-def test_print():
-    print("test")
