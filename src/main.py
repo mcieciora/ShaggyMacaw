41b15655ce1deff11fe8c6ece1e2a68a9d1244cd
mcieciora <mcieciora@nowhere.com>
2024-04-01 18:19:23 +0200

    01042024-1811

--- src/main.py
+++ src/main.py
@@ -1,9 +1,12 @@
+"""main.py."""
+
 from requests import get
 
 
 def get_google_com_status_code():
     """
     Get https://google.com status code.
+
     :return: Status code
     """
     return get(url="https://google.com", timeout=5).status_code
