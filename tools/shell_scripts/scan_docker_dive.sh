958497c53e5e84205f005efb9f5973ef33ae60ea
mcieciora <mcieciora@nowhere.com>
2024-04-29 22:27:28 +0200

    29042024-2227

--- tools/shell_scripts/scan_docker_dive.sh
+++ tools/shell_scripts/scan_docker_dive.sh
@@ -5,5 +5,5 @@ DOCKERHUB_REPO="mcieciora/careless_vaquita"
 TAGS="latest test_image"
 for TAG in $TAGS; do
   echo "Running docker dive on $TAG image"
-  docker run --rm -v /var/run/docker.sock:/var/run/docker.sock wagoodman/dive $DOCKERHUB_REPO:"$TAG" > scan_dive_"$TAG".txt
+  docker run --rm -v /var/run/docker.sock:/var/run/docker.sock wagoodman/dive --ci $DOCKERHUB_REPO:"$TAG" > scan_dive_"$TAG".txt
 done
