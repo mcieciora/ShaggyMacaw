26b33c79550330aaa3ecad77ad2a5b75631a7351
mcieciora <mcieciora@nowhere.com>
2024-04-29 15:29:44 +0200

    29042024-1529

--- tools/shell_scripts/scan_docker_images.sh
+++ tools/shell_scripts/scan_docker_images.sh
@@ -10,7 +10,7 @@ for TAG in $TAGS; do
   SCAN_RESULT=$?
   if [ "$SCAN_RESULT" -ne 0 ]; then
     echo "Vulnerabilities found. Running recommendations"
-      docker run --rm -e DOCKER_SCOUT_HUB_USER="$USERNAME" -e DOCKER_SCOUT_HUB_PASSWORD="$PASSWORD" -v /var/run/docker.sock:/var/run/docker.sock docker/scout-cli recommendations $DOCKERHUB_REPO:"$TAG" > scan_"$TAG".txt
+    docker run --rm -e DOCKER_SCOUT_HUB_USER="$USERNAME" -e DOCKER_SCOUT_HUB_PASSWORD="$PASSWORD" -v /var/run/docker.sock:/var/run/docker.sock docker/scout-cli recommendations $DOCKERHUB_REPO:"$TAG" > scan_"$TAG".txt
     grep "This image version is up to date" scan_"$TAG".txt
     IMAGE_UP_TO_DATE=$?
     grep "There are no tag recommendations at this time" scan_"$TAG".txt
@@ -19,6 +19,7 @@ for TAG in $TAGS; do
       RETURN_VALUE=0
     else
       RETURN_VALUE=1
+      echo "Script failed, because vulnerabilities were found. Please fix them according to given recommendations."
     fi
   else
     echo "No vulnerabilities detected"
