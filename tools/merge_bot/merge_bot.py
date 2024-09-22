334d6b340a32238730ab39acd0f646fda7c71f5f
mcieciora <mcieciora@nowhere.com>
2024-09-22 18:06:19 +0200

    22092024-1806

--- tools/merge_bot/merge_bot.py
+++ tools/merge_bot/merge_bot.py
@@ -1,7 +1,7 @@
 from os import environ
+from requests import get, post, put
 from sys import argv
 from argparse import ArgumentParser
-from requests import get, post, put
 
 
 class MergeBotAPI:
@@ -9,76 +9,63 @@ class MergeBotAPI:
 
     def __init__(self):
         self.api_url = environ["GITHUB_API_URL"]
-
-    def get_branch_pull_number(self, branch_name):
-        """
-        Check if branch has related pull request.
-
-        :return: Pull request number if exists else False
-        """
-        get_pull_requests = get(f"{self.api_url}/pulls", timeout=15)
-        for pull_request in get_pull_requests.json():
-            if pull_request["head"]["ref"] == branch_name:
-                return pull_request["number"]
+        self.headers = {"Authorization": f"Bearer {environ['GITHUB_API_TOKEN']}",
+                        "Accept": "application/vnd.github+json",
+                        "X-GitHub-Api-Version": "2022-11-28"}
 
     def create_pull_request(self, branch_name, base_branch):
         """
-        Create pull request via Github API call.
+        Create pull request via GitHub API call.
 
         :return: None
         """
-        if self.get_branch_pull_number(branch_name):
-            print(f"Pull request for {branch_name} already exists.")
+        response = get(f"{self.api_url}/pulls", headers=self.headers, timeout=15)
+        for pull_request in response.json():
+            if pull_request["head"]["ref"] == branch_name:
+                print(f"Pull request for {branch_name} already exists.")
+                return False
+        json = {"title": f"Merge {branch_name}",
+                "body": f"Automatically created pull request that merges {branch_name} into {base_branch}.",
+                "head": f"{environ['GITHUB_USER']}:{branch_name}",
+                "base": base_branch}
+        response = post(f"{self.api_url}/pulls", headers=self.headers, json=json, timeout=15)
+        if response.status_code == 201:
+            print("Pull request creation succeeded.")
         else:
-            headers = {"Authorization": f"Bearer {environ['GITHUB_API_TOKEN']}",
-                       "Accept": "application/vnd.github+json",
-                       "X-GitHub-Api-Version": "2022-11-28"}
-            json = {"title": f"Merge {branch_name}",
-                    "body": f"Automatically created pull request that merges {branch_name} into {base_branch}.",
-                    "head": f"{environ['GITHUB_USER']}:{branch_name}",
-                    "base": base_branch}
-            post_response = post(f"{self.api_url}/pulls", headers=headers, json=json, timeout=15)
-            if post_response.status_code == 201:
-                print("Pull request creation succeeded.")
-            else:
-                print(f"API call failed. {post_response.reason}")
+            print(f"API call failed. {response.reason}")
 
-    def _delete_branch(self, branch_name):
+    def merge_pull_request(self):
         """
-        Delete branch via Github API call.
+        Get all active pull requests via GitHub API call.
 
         :return: None
         """
-        pass
-
-    def merge_branch(self, branch_name):
-        """
-        Merge pull request via Github API call.
-
-        :return: None
-        """
-        pull_number = self.get_branch_pull_number(branch_name)
-        if pull_number:
-            headers = {"Authorization": f"Bearer {environ['GITHUB_API_TOKEN']}",
-                       "Accept": "application/vnd.github+json",
-                       "X-GitHub-Api-Version": "2022-11-28"}
-            merge_data = {"commit_title": f"Merge #{pull_number}",
-                          "commit_message": f"{branch_name} successfully merged by MergeBot.",
-                          "merge_method": "squash"}
-            merge_response = put(f"{self.api_url}/pulls/{pull_number}/merge", headers=headers, json=merge_data,
-                                 timeout=15)
-            if merge_response.status_code == 200:
-                self._delete_branch(branch_name)
-                print("Branch merged successfully.")
-            else:
-                print(f"API call failed. {merge_response.reason}")
+        response = get(f"{self.api_url}/pulls", headers=self.headers, timeout=15)
+        response_json = response.json()
+        if not response_json:
+            print("No pull requests in the queue.")
+            return False
+        for pull_request in response.json():
+            pull_number = pull_request['number']
+            response = get(f"{self.api_url}/pulls/{pull_number}/reviews", headers=self.headers, timeout=15)
+            if response.json()["state"] == "APPROVED":
+                merge_data = {"commit_title": f"Merge #{pull_number}",
+                              "commit_message": f"#{pull_number} successfully merged by MergeBot.",
+                              "merge_method": "squash"}
+                merge_response = put(f"{self.api_url}/pulls/{pull_number}/merge", headers=self.headers, json=merge_data,
+                                     timeout=15)
+                if merge_response.status_code == 200:
+                    print(f"Pull request #{pull_number} merged successfully.")
 
 
 if __name__ == '__main__':
+    from dotenv import load_dotenv
+    load_dotenv("../../.env")
     parser = ArgumentParser()
-    parser.add_argument("--create", help="Create pull request. Usage: merge_bot.py --create [--branch] branch "
-                                         "name\n[--base] base branch", action="store_true")
-    parser.add_argument("--merge", help="Merge branch. Usage: merge_bot.py --merge <branch_name>")
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument("--create", help="Create pull request. Usage: merge_bot.py --create [--branch] branch "
+                                        "name\n[--base] base branch", action="store_true")
+    group.add_argument("--merge", action="store_true", help="Merge branch. Usage: merge_bot.py --merge")
     if "--create" in argv:
         parser.add_argument("--branch", dest="branch_name", required=True, help="Branch name")
         parser.add_argument("--base", dest="base_branch", required=True, help="Base branch")
@@ -88,4 +75,4 @@ if __name__ == '__main__':
     if args.create:
         merge_bot_api.create_pull_request(args.branch_name, args.base_branch)
     else:
-        merge_bot_api.merge_branch(args.merge)
+        merge_bot_api.merge_pull_request()
