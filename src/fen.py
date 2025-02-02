91da49f2c952fa488b840f2e3de1222bfb78ea21
mcieciora <michalcieciora96@gmail.com>
2025-02-02 19:56:48 +0100

    02022025-1956

--- src/fen.py
+++ src/fen.py
@@ -64,7 +64,8 @@ class Fen:
                 generated_squares.append(f"{file}{rank}")
         return generated_squares
 
-    def parse_board_setup(self, fen):
+    @staticmethod
+    def parse_board_setup(fen):
         """Parse board setup and verify number of files and ranks."""
         return_board = []
         ranks = fen.split("/")
@@ -92,11 +93,13 @@ class Fen:
     @staticmethod
     def parse_active_colour(active_colour):
         """Parse and verify active colours value."""
-        if active_colour not in ["w", "b"]:
-            raise WrongActiveColourValue(
-                f"Active colour has incorrect value: {active_colour}, expected: w/b"
-            )
-        return active_colour == "w"
+        if active_colour in ["w", "b"]:
+            return active_colour == "w"
+        elif type(active_colour) is bool:
+            return {True: "w", False: "b"}[active_colour]
+        else:
+            raise WrongActiveColourValue(f"Active colour has incorrect value: {active_colour}, "
+                                         f"expected: w/b or True/False")
 
     @staticmethod
     def parse_castling_rights(castling_rights):
@@ -146,6 +149,28 @@ class Fen:
             raise NotIntegerFullMoveValue(
                 f"Full move value is not an integer: {full_move_value}"
             ) from exc
+
+    def regenerate_fen(self):
+        """Generate FEN from existing configuration."""
+        ranks = []
+        for rank in reversed(self.board_setup):
+            temp_rank = ''
+            empty_count = 0
+            for square in rank:
+                if square.value == 0:
+                    empty_count += 1
+                else:
+                    if empty_count > 0:
+                        temp_rank += str(empty_count)
+                    temp_rank += square.value
+                    empty_count = 0
+            if empty_count > 0:
+                temp_rank += str(empty_count)
+            ranks.append(temp_rank)
+
+        board = "/".join(ranks)
+        return (f"{board} {self.parse_active_colour(self.active_colour)} "
+                f"{self.castling_rights} {self.available_en_passant} {self.half_move_clock} {self.full_move_number}")
 
 
 class WrongBoardSize(Exception):
