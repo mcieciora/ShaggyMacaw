13f556a21f42fff29bfe0d1c5cb2f77a1c628e44
mcieciora <michalcieciora96@gmail.com>
2025-02-02 16:31:46 +0100

    02022025-1631

--- automated_tests/unittest/test_piece_is_pawn_next_move_promotion.py
+++ automated_tests/unittest/test_piece_is_pawn_next_move_promotion.py
@@ -4,21 +4,21 @@ from src.piece import Pawn
 
 
 @mark.unittest
-def test__unittest__chess_board__is_pawn_next_move_promotion__white_promotion():
+def test__unittest__piece__is_pawn_next_move_promotion__white_promotion():
     test_object = Pawn("P", (0, 6))
     actual_data = test_object.is_pawn_next_move_promotion()
     assert actual_data is True, f"Expected: True, actual: {actual_data}"
 
 
 @mark.unittest
-def test__unittest__chess_board__is_pawn_next_move_promotion__black_promotion():
+def test__unittest__piece__is_pawn_next_move_promotion__black_promotion():
     test_object = Pawn("p", (0, 1))
     actual_data = test_object.is_pawn_next_move_promotion()
     assert actual_data is True, f"Expected: True, actual: {actual_data}"
 
 
 @mark.unittest
-def test__unittest__chess_board__is_pawn_next_move_promotion__no_promotion_available():
+def test__unittest__piece__is_pawn_next_move_promotion__no_promotion_available():
     test_object = Pawn("p", (0, 5))
     actual_data = test_object.is_pawn_next_move_promotion()
     assert actual_data is False, f"Expected: False, actual: {actual_data}"
