d1a139e99158ba3db4490f1943c6ed5f31b39da1
mcieciora <michalcieciora96@gmail.com>
2025-02-24 21:16:32 +0100

    24022025-2116

---
+++ automated_tests/unittest/test_chess_board_in_check.py
@@ -0,0 +1,21 @@
+from pytest import mark
+
+from src.chess_board import ChessBoard
+
+
+@mark.unittest
+def test__unittest__chess_board__in_check__no_check():
+    original_fen = "2b1r1k1/1p3pp1/2n5/2N5/Q2p1NPp/7P/PP2P1B1/2q2RK1 b - - 2 28"
+    test_object = ChessBoard(original_fen)
+    test_object.generate_all_possible_moves()
+    actual_data = test_object.in_check
+    assert actual_data == False, f"Expected: False, actual: {actual_data}"
+
+
+@mark.unittest
+def test__unittest__chess_board__in_check__check():
+    original_fen = "1r1q1rk1/Q2npp1p/6p1/2P2b2/8/4BN2/PbP2PPP/2KR1B1R w - - 0 13"
+    test_object = ChessBoard(original_fen)
+    test_object.generate_all_possible_moves()
+    actual_data = test_object.in_check
+    assert actual_data == True, f"Expected: True, actual: {actual_data}"
