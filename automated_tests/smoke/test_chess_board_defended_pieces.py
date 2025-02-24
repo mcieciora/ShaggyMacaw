f313cdf72f12b4afd0790a6feafbc8e51cdf15ee
mcieciora <michalcieciora96@gmail.com>
2025-02-24 18:46:10 +0100

    24022025-1845

---
+++ automated_tests/smoke/test_chess_board_defended_pieces.py
@@ -0,0 +1,61 @@
+from glob import glob
+
+from pytest import mark
+
+from src.chess_board import ChessBoard
+
+test_data_dict = {
+    "test_resource_1": {
+        "fen": "rn1q1rk1/pb1pppbp/1p3np1/2P5/8/1P2PN2/PBP1BPPP/RN1Q1RK1 b - - 0 8",
+        "expected_result": {
+            True: ["a1", "a2", "b1", "b3", "c2", "d1", "e2", "e3", "f1", "f2", "f3", "g1", "g2", "h2"],
+            False: ["a8", "a7", "b8", "b6", "d8", "d7", "e7", "f8", "f7", "f6", "g8", "g7", "g6", "h7"]
+        }
+    },
+    "test_resource_2": {
+        "fen": "r2q1rk1/pb3pbp/3ppnp1/2p1n3/2P5/1PN1P3/PBQNBPPP/R4RK1 b - - 1 13",
+        "expected_result": {
+            True: ["a1", "a2", "b2", "b3", "c3", "c4", "d2", "e2", "e3", "f1", "f2", "g1", "g2", "h2"],
+            False: ["a8", "a7", "c5", "d6", "d8", "e5", "e6", "f6", "f7", "f8", "g6", "g7", "g8", "h7"]
+        }
+    },
+    "test_resource_3": {
+        "fen": "2rr2k1/pb2qpbp/5np1/2ppn3/8/1PN1P2P/PBQNBPP1/3RR1K1 w - - 0 18",
+        "expected_result": {
+            True: ["b2", "b3", "c3", "d1", "d2", "e1", "e2", "e3", "f2", "g2", "h3"],
+            False: ["c8", "c5", "d8", "d5", "e5", "f7", "f6", "g7", "g6", "h7"]
+        }
+    },
+    "test_resource_4": {
+        "fen": "r3r1k1/pp3p1p/1bn5/6p1/3p1Nb1/6P1/PP1NPPBP/R1R2K2 w - g6 0 18",
+        "expected_result": {
+            True: ["a1", "a2", "c1", "e2", "f2", "f4", "g2", "g3"],
+            False: ["a8", "a7", "b6", "c6", "d4", "e8", "f7", "g8", "h7"]
+        }
+    },
+    "test_resource_5": {
+        "fen": "2r3k1/p1r2p1p/6p1/2N1R3/2N5/1P5P/P4P2/6K1 w - - 0 31",
+        "expected_result": {
+            True: ["a2", "b3", "c4", "c5", "e5", "f2", "g1"],
+            False: ["a7", "c7", "c8", "f7", "g6", "g8", "h7"]
+        }
+    }
+}
+
+
+def get_parametrized_test_set():
+    parametrized_test_set_list = []
+    for test_key, test_data in test_data_dict.items():
+        parametrized_test_set_list.append((test_key, test_data, test_data["expected_result"]))
+    return parametrized_test_set_list
+
+
+@mark.smoke
+@mark.parametrize("test_key,test_data,expected_output", get_parametrized_test_set(), ids=test_data_dict.keys())
+def test__smoke__chess_board__defended_pieces(test_key, test_data, expected_output):
+    test_object = ChessBoard(test_data["fen"])
+    test_object.generate_all_possible_moves()
+    for active_colour in test_object.defended_pieces:
+        actual_data = test_object.defended_pieces[active_colour]
+        expected_data = expected_output[active_colour]
+        assert actual_data == expected_data, f"Expected: {expected_data}, actual: {actual_data}."
--- automated_tests/smoke/test_chess_board_get_all_possible_moves.py
+++ automated_tests/smoke/test_chess_board_get_all_possible_moves.py
@@ -1,32 +1,71 @@
+from glob import glob
 from pytest import mark
 
 from src.chess_board import ChessBoard
 
 test_data_dict = {
     "test_resource_1": {
-        "fen": "r1bqk1nr/1p3pbp/p1np2p1/4p3/2P1P3/2NB4/PP3PPP/RNBQK2R w KQkq - 2 9",
-        "active_colour": True,
-        "expected_result": {
-            True: 39,
-            False: 39
-        }
+        True: 41,
+        False: 31
+    },
+    "test_resource_2": {
+        True: 42,
+        False: 39
+    },
+    "test_resource_3": {
+        True: 43,
+        False: 40
+    },
+    "test_resource_4": {
+        True: 37,
+        False: 42
+    },
+    "test_resource_5": {
+        True: 35,
+        False: 20
+    },
+    "test_resource_6": {
+        True: 37,
+        False: 25
+    },
+    "test_resource_7": {
+        True: 39,
+        False: 39
+    },
+    "test_resource_8": {
+        True: 48,
+        False: 32
+    },
+    "test_resource_9": {
+        True: 43,
+        False: 35
+    },
+    "test_resource_10": {
+        True: 30,
+        False: 20
     }
 }
 
 
-def get_parametrized_test_set():
+def get_parametrized_test_set(test_file):
     parametrized_test_set_list = []
-    for test_key, test_data in test_data_dict.items():
-        parametrized_test_set_list.append((test_key, test_data, test_data["expected_result"]))
+    full_test_file_path = glob(f"automated_tests/test_data/{test_file}")[0]
+    with open(full_test_file_path, mode="r", encoding="utf-8") as test_fen_file:
+        for index, line in enumerate(test_fen_file.readlines()):
+            test_object = ChessBoard(line)
+            actual_data = {}
+            for active_colour in [True, False]:
+                actual_data[active_colour] = [str(move) for move in test_object.generate_all_possible_moves()
+                               if move.active_colour is active_colour]
+            parametrized_test_set_list.append((actual_data, test_data_dict[f"test_resource_{index+1}"]))
     return parametrized_test_set_list
 
 
 @mark.smoke
-@mark.parametrize("test_key,test_data,expected_output", get_parametrized_test_set(), ids=test_data_dict.keys())
-def test__smoke__chess_board__generate_all_possible_moves(test_key, test_data, expected_output):
-    test_object = ChessBoard(test_data["fen"])
-    for active_colour, expected_value in test_data['expected_result'].items():
-        actual_data = [str(move) for move in test_object.generate_all_possible_moves()
-                       if move.active_colour is active_colour]
-        assert len(actual_data) == test_data['expected_result'][active_colour], \
-            f"Expected: {test_data['expected_result']}, actual: {len(actual_data)}"
+@mark.parametrize("test_data,expected_output", get_parametrized_test_set("fen_0"), ids=test_data_dict.keys())
+def test__smoke__chess_board__generate_all_possible_moves(test_data, expected_output):
+    for active_colour in test_data:
+        actual_data = len(test_data[active_colour])
+        expected_data = expected_output[active_colour]
+        assert actual_data == expected_data, (f"Expected: {expected_data}, actual: {actual_data}. "
+                                              f"Generated moves: {test_data[active_colour]}")
