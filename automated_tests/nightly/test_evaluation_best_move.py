cd32b4c74570109be60d3e5f44192c362429fbfd
mcieciora <michalcieciora96@gmail.com>
2025-06-16 17:09:11 +0200

    16062025-1709

---
+++ automated_tests/nightly/test_evaluation_best_move.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+from glob import glob
+from json import dumps
+from pytest import mark
+
+from src.chess_board import ChessBoard
+from src.evaluation import Evaluation
+
+
+@mark.nightly
+def test__nightly__evaluation__measure_get_best_move_runtime():
+    fen_files = glob(f"../../automated_tests/test_data/*")
+    runtime_results_map = {}
+    for fen_file in fen_files:
+        with open(fen_file, mode="r", encoding="utf-8") as test_fen_file:
+            for line in test_fen_file.readlines():
+                chess_board = ChessBoard(line)
+                test_object = Evaluation(chess_board)
+                start = datetime.now()
+                test_object.get_best_move(10)
+                runtime = datetime.now() - start
+                runtime_results_map[test_object.chess_board.fen.current_fen] = runtime.microseconds
+    with open("measure_get_best_move_runtime.json", mode="w") as result_file:
+        result_file.writelines(dumps(runtime_results_map))
