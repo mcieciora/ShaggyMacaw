1c0dc612ab31d43313fa34d7f491db0276a555f7
mcieciora <michalcieciora96@gmail.com>
2025-06-16 17:57:01 +0200

    16062025-1756

--- automated_tests/nightly/test_evaluation_best_move.py
+++ automated_tests/nightly/test_evaluation_best_move.py
@@ -6,6 +6,7 @@ from pytest import mark
 from src.chess_board import ChessBoard
 from src.evaluation import Evaluation
 
+from os import  listdir, curdir
 
 @mark.nightly
 def test__nightly__evaluation__measure_get_best_move_runtime():
@@ -20,5 +21,7 @@ def test__nightly__evaluation__measure_get_best_move_runtime():
                 test_object.get_best_move(10)
                 runtime = datetime.now() - start
                 runtime_results_map[test_object.chess_board.fen.current_fen] = runtime.microseconds
+    print(curdir)
+    print(listdir(curdir))
     with open("results/measure_get_best_move_runtime.json", mode="w", encoding="utf-8") as result_file:
         result_file.writelines(dumps(runtime_results_map))
